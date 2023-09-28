e26fe6576a3aa0947417a8f52f278d431b5b0e46
rahulsharma-rks <rahulsharma.rks31@gmail.com>
2023-09-28 13:17:21 +0530

    JSON and Web Storage

---
+++ WebStorage/WebStorage.js
@@ -0,0 +1,15 @@
+/*
+HTML web storage provides two objects for storing data on the client:
+
+window.localStorage - stores data with no expiration date
+window.sessionStorage - stores data for one session (data is lost when the browser tab is closed)Most modern browsers support Web Storage, however it is good to check browser support for localStorage and sessionStorage. Let us see the available methods for the Web Storage objects.
+Web Storage objects:
+
+localStorage - to display the localStorage object
+localStorage.clear() - to remove everything in the local storage
+localStorage.setItem() - to store data in the localStorage. It takes a key and a value parameters.
+localStorage.getItem() - to display data stored in the localStorage. It takes a key as a parameter.
+localStorage.removeItem() - to remove stored item form a localStorage. It takes key as a parameter.
+localStorage.key() - to display a data stored in a localStorage. It takes index as a parameter.
+
+*/
