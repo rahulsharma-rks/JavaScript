e155b83cbfa46318e5fc77c842fb616f5f07e98e
Rahul Kumar Sharma <rahulsharma.rks31@gmail.com>
2024-03-24 21:23:50 +0530

    For/In/Of/While/DoWhile

--- forLoop.js
+++ forLoop.js
@@ -1,5 +1,5 @@
 for (let i = 0; i < 10; i++) {
-    console.log(i);
+    //console.log(i);
 }
 
 var user = [
@@ -11,8 +11,35 @@ var user = [
     "TChalla"
 ];
 
-for (let i = 0; i < user.length; i++){
-    if (typeof user[i] !== "string") continue;
-    console.log(user[i]);
+// for (let i = 0; i < user.length; i++){
+//     if (typeof user[i] !== "string") continue;
+//     console.log(user[i]);
+// }
+
+// ForEach
+user.forEach(u => (console.log(u)));
+
+// For Of -->Mainly used for array
+const name = [
+    "YouTube",
+    "Meta",
+    "Instagram",
+    "Whatsapp",
+    "Netflix"
+]
+
+for (const n of name){
+    console.log(n);
 }
 
+// For In  -->Mainly used for object, OutPut is key
+const logo ={
+    car: "BMW",
+    isp: "Airtel",
+    travel: "Scorpio",
+    hotel: "Taj"
+}
+for (const l in logo){
+    console.log(l);
+    //console.log(logo[l]);
+}
---
+++ whileDoWhile.js
@@ -0,0 +1,20 @@
+var user = [
+    "Tom", 
+    "Mark",
+    "Tony",
+    1997,
+    "Steave",
+    "TChalla"
+];
+
+// let i = 0;
+// while (i < user.length) {
+//     console.log(user[i]);
+//     i++;
+// }
+
+let i = 0;
+do {
+    console.log(user[i]);
+    i++;
+}   while (i < user.length)
